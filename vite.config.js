f954b4ccedb3b1086cdc683fa53e3f286ffb911e
No Limit Nexus <120111350+NoLimitNexus@users.noreply.github.com>
2026-05-03 07:31:58 -0600

    Fix deployment dependencies and case conflicts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   base: './',
   build: {
     outDir: 'dist',
@@ -11,11 +13,11 @@ export default defineConfig({
         blobMovement: resolve(__dirname, 'src/characters/BlobMovement/index.html'),
         modularMan: resolve(__dirname, 'src/characters/ModularMan/index.html'),
         projectiles: resolve(__dirname, 'src/effects/Projectiles/index.html'),
-        spells: resolve(__dirname, 'src/effects/Spells/INDEX.HTML'),
+        spells: resolve(__dirname, 'src/effects/Spells/spells.html'),
         fogMachine: resolve(__dirname, 'src/environments/FogMachine/index.html'),
         fogWater: resolve(__dirname, 'src/environments/FogWater/index.html'),
         terrainEditor: resolve(__dirname, 'src/environments/TerrainEditor/index.html'),
-        worldGenerator: resolve(__dirname, 'src/environments/WorldGenerator/INDEX.HTML'),
+        worldGenerator: resolve(__dirname, 'src/environments/WorldGenerator/worldgen.html'),
         actionGame: resolve(__dirname, 'src/sandbox/ActionGame/index.html'),
         unifiedSandbox: resolve(__dirname, 'src/sandbox/index.html')
       }
